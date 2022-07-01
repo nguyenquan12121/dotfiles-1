db33c648ad58b4d2f88a4de2e141693d26a0e656
Clay Gomera <misterclay@tutanota.com>
2022-07-01 12:04:37 -0400

    Updated

--- user/.config/suckless/dwmblocks/blocks.def.h
+++ user/.config/suckless/dwmblocks/blocks.def.h
@@ -1,15 +1,15 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Command*/	 	                             /*Update Interval*/	/*Update Signal*/
-	{"", "$HOME/.config/dwmblocks/scripts/memory",	        1,		            1},
+	{"", "$HOME/.config/suckless/dwmblocks/scripts/memory",           1,                       1},
 
-	{"", "$HOME/.config/dwmblocks/scripts/layout",	        1,		            1},
+	{"", "$HOME/.config/suckless/dwmblocks/scripts/layout",           1,                       1},
 
-	{"", "$HOME/.config/dwmblocks/scripts/battery",         1,		            1},
+	{"", "$HOME/.config/suckless/dwmblocks/scripts/battery",          1,                       1},
 
-	{"", "$HOME/.config/dwmblocks/scripts/volume",			1,		            1},
+	{"", "$HOME/.config/suckless/dwmblocks/scripts/volume",           1,                       1},
 
-	{"", "$HOME/.config/dwmblocks/scripts/clock",			5,		            0},
+	{"", "$HOME/.config/suckless/dwmblocks/scripts/clock",            5,                       0},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
